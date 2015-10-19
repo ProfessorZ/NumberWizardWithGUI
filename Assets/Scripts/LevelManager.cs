2ba8f784f1d56c0d51ceab81a395273c7411225d
ProfessorZ <tom.geldermans@gmail.com>
2015-10-19 08:26:52 +0100

    Added Game-screen UI
    Branched to development, added return to start-button in gamescreen.
    Added level-loading and application-quitting functionality in
    LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,11 +6,13 @@ public class LevelManager : MonoBehaviour {
 	//Load new level, passes along the name of the level to be loaded
 	public void loadNewLevel(string name){
 		Debug.Log ("Level load requested, for:" + name);
-
+		Application.LoadLevel(name);
 	}
 
 	public void quitRequest(){
 		Debug.Log("Quit requested... should we exit?");
+		Application.Quit ();
+
 	}
 
 
